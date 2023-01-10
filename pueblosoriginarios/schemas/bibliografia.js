775d588148fa7172a0a19e1b5b3c7dcd60afc51d
erimasini2 <121623405+erimasini2@users.noreply.github.com>
2023-01-10 11:59:25 -0300

    fix Bibliografia component

--- pueblosoriginarios/schemas/bibliografia.js
+++ pueblosoriginarios/schemas/bibliografia.js
@@ -1,25 +1,33 @@
 export default {
-    name: "bibiliografia",
-    type: "document",
-    title: "Bibliografía",
-    fields: [
-      {
-        name: "title",
-        type: "string",
-        title: "Titulo",
-      },
-     {
-        name: "bibliografia",
-        type: "array",
-        title: "Bibliografía",
-        of: [
-          {
-            type: "componentbibliografia",
-            title: "Componentes de página Bibliografía",
-          },
-         
+  name: "componentbibliografia",
+  type: "object",
+  title: "Bibliografía Componentes",
+  fields: [
+    {
+      name: "tipoBibliografia",
+      type: "string",
+      title: "Bibliografía",
+      options: {
+        list: [
+          { title: "Guaraní", value: "guarani" },
+          { title: "Chané", value: "chane" },
         ],
       },
-    ],
-  };
+    },
+    { type: "string", name: "title", title: "Titulo" },
+    { type: "string", name: "author", title: "Autor" },
+    { type: "richTextBody", name: "richTextBody", title: "Bloque de Texto" },
+    {
+      type: "categoryReference",
+      name: "categoryReferenceBibliografia",
+      title: "Categoría",
+    },
+    {
+      type: "reference",
+      name: "referenceBibliografia",
+      title: "PDF",
+      to: { type: "pdf" },
+    },
+  ],
+};
   
