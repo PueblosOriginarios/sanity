d446d489882e6397af493d6ac065d940455b70ee
erimasini2 <121623405+erimasini2@users.noreply.github.com>
2023-01-13 17:01:15 -0300

    add header Image

--- pueblosoriginarios/schemas/bibliografia.js
+++ pueblosoriginarios/schemas/bibliografia.js
@@ -9,6 +9,11 @@ export default {
       title: "Titulo",
     },
     {
+      name: "imageHeader",
+      type: "imageComponent",
+      title: "Image Header",
+    },
+    {
       name: "tipoBibliografia",
       type: "string",
       title: "Tipo de Bibliografia",
