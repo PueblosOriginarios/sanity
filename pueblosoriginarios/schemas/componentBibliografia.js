8078ab80ce6518d097ed19a527d24ff5ad7f986a
Drina <drina.rincon@bitlogic.io>
2023-01-25 13:36:26 -0300

    fix image

--- pueblosoriginarios/schemas/componentBibliografia.js
+++ pueblosoriginarios/schemas/componentBibliografia.js
@@ -1,34 +1,34 @@
 export default {
-    name: "componentbibliografia",
-    type: "document",
-    title: "Bibliografía Componentes",
-    fields: [
-      { type: "string", name: "title", title: "Titulo" },
-      { type: "string", name: "author", title: "Autor" },
-      { type: "richTextBody", name: "richTextBody", title: "Bloque de Texto" },
-      {
-        type: "reference",
-        name: "referenceBibliografia",
-        title: "PDF",
-        to: { type: "pdf" },
+  name: "componentbibliografia",
+  type: "document",
+  title: "Bibliografía Componentes",
+  fields: [
+    { type: "string", name: "title", title: "Titulo" },
+    { type: "string", name: "author", title: "Autor" },
+    { type: "richTextBody", name: "richTextBody", title: "Bloque de Texto" },
+    {
+      type: "reference",
+      name: "referenceBibliografia",
+      title: "PDF",
+      to: { type: "pdf" },
+    },
+    {
+      name: "image",
+      type: "imageComponent",
+      title: "Imagen de bibliografía",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "filtroBilbliografia",
+      type: "string",
+      title: "Pueblo",
+      options: {
+        list: [
+          { title: "Guaraní", value: "guarani" },
+          { title: "Chané", value: "chane" },
+          { title: "Ambos", value: "ambos" },
+        ],
       },
-      {
-        name: "image",
-        type: "image",
-        title: "Imagen de bibliografía",
-        validation: (Rule) => Rule.required(),
-      },
-      {
-        name: "filtroBilbliografia",
-        type: "string",
-        title: "Pueblo",
-        options: {
-          list: [
-            { title: "Guaraní", value: "guarani" },
-            { title: "Chané", value: "chane" },
-            { title: "Ambos", value: "ambos" },
-          ],
-        },
-      },
-    ],
-  };
+    },
+  ],
+};
